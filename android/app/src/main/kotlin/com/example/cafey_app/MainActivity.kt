6429570603643f03f73489a776daad97ddf81f31
NelMatrix743 <nelmatrix155@gmail.com>
2025-10-06 15:33:20 +0000

    genesis: first commit

---
+++ android/app/src/main/kotlin/com/example/cafey_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cafey_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CafeyApp());
+}
+
+class CafeyApp extends StatelessWidget {
+  const CafeyApp({super.key});
+}
