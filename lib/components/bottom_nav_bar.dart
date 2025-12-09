03a5f937f386693d3c5bf1c7dca78e9ac5aa1141
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2025-12-09 10:07:52 +0000

    feat(ui): implement native material3 nav bar

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -1,2 +1,27 @@
+import "package:cafey_app/utils/const_properties.dart";
+import "package:cafey_app/utils/image_assets.dart";
 import "package:flutter/material.dart";
-import "package:google_nav_bar/google_nav_bar.dart";
+
+class CafeyBottomNavBar extends StatelessWidget {
+  const CafeyBottomNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      indicatorColor: mainBackgroundColor,
+
+      destinations: const <Widget>[
+        NavigationDestination(
+          icon: ImageIcon(homeOutlinedIcon),
+          selectedIcon: ImageIcon(homeFilledIcon, color: Colors.brown),
+          label: "Home",
+        ),
+        NavigationDestination(
+          icon: ImageIcon(shoppingCartOutlinedIcon),
+          selectedIcon: ImageIcon(shoppingCartFilledIcon, color: Colors.brown),
+          label: "Cart",
+        ),
+      ],
+    );
+  }
+}
