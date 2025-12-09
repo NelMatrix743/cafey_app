2756435157ddf2befa825443ccdcba40b10b9367
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2025-12-09 14:04:09 +0000

    feat(ui): change home screen widget to stateful widget, and implement cafey bottom nav bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,20 @@
+import "package:cafey_app/components/bottom_nav_bar.dart";
 import "package:cafey_app/utils/const_properties.dart";
 import "package:flutter/material.dart";
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: mainBackgroundColor);
+    return Scaffold(
+      backgroundColor: mainBackgroundColor,
+      bottomNavigationBar: CafeyBottomNavBar(),
+    );
   }
 }
