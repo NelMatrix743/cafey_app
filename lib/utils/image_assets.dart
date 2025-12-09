a699010e8e4bfb185915e6d4212c80e0fed03810
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2025-12-09 10:10:32 +0000

    feat(ui): map const variables to icon assets for cleaner access

---
+++ lib/utils/image_assets.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+
+const String assetDirPath = "assets/icons/";
+
+// consts endpoints for icon assets
+const shoppingCartOutlinedIcon = AssetImage(
+  "${assetDirPath}shopping_cart_outlined.png",
+);
+const shoppingCartFilledIcon = AssetImage("${assetDirPath}shopping");
+
+const homeOutlinedIcon = AssetImage("${assetDirPath}home_outlined.png");
+const homeFilledIcon = AssetImage("${assetDirPath}home_filled.png");
