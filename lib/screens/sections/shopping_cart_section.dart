88345ab10ce7260a90076d0a4dc1166eb1710231
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2025-12-09 15:01:01 +0000

    feat(ui): add source files for home section and shopping cart section

---
+++ lib/screens/sections/shopping_cart_section.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+class ShoppingCartSection extends StatelessWidget {
+  const ShoppingCartSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
