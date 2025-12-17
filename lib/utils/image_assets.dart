d3dd2a55e13514f780135723d74855a596fedd60
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2025-12-17 06:19:58 +0000

    feat(assets): add image assets for available coffee products

--- lib/utils/image_assets.dart
+++ lib/utils/image_assets.dart
@@ -3,6 +3,9 @@ import "package:flutter/material.dart";
 const String assetDirPath = "assets/icons/";
 
 // consts endpoints for icon assets
+const homeOutlinedIcon = AssetImage("${assetDirPath}home_outlined.png");
+const homeFilledIcon = AssetImage("${assetDirPath}home_filled.png");
+
 const shoppingCartOutlinedIcon = AssetImage(
   "${assetDirPath}shopping_cart_outlined.png",
 );
@@ -10,5 +13,28 @@ const shoppingCartFilledIcon = AssetImage(
   "${assetDirPath}shopping_cart_filled.png",
 );
 
-const homeOutlinedIcon = AssetImage("${assetDirPath}home_outlined.png");
-const homeFilledIcon = AssetImage("${assetDirPath}home_filled.png");
+// long black
+const longBlackNormalAssetIcon = AssetImage(
+  "${assetDirPath}black_coffee_normal.png",
+);
+const longBlackSharpAssetIcon = AssetImage(
+  "${assetDirPath}black_coffee_sharp.png",
+);
+
+// espresso
+const espressoNormalAssetIcon = AssetImage(
+  "${assetDirPath}espresso_normal.png",
+);
+const espressoSharpAssetIcon = AssetImage("${assetDirPath}espresso_sharp.png");
+
+// latte
+const latteNormalAssetIcon = AssetImage("${assetDirPath}latte_normal.png");
+const latteSharpAssetIcon = AssetImage("${assetDirPath}latte_sharp.png");
+
+// iced coffee
+const icedCoffeeNormalAssetIcon = AssetImage(
+  "${assetDirPath}ice_coffee_normal.png",
+);
+const icedCoffeeSharpAssetIcon = AssetImage(
+  "${assetDirPath}ice_coffee_sharp.png",
+);
