8ed00fd584ded1c8c3aeb1635ef3bc4be640075c
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2025-12-17 23:48:31 +0000

    feat(ui): implement a custom list tile for available coffee product in store

---
+++ lib/components/coffee_tile.dart
@@ -0,0 +1,34 @@
+import "package:cafey_app/models/coffee.dart";
+import "package:flutter/material.dart";
+
+class CoffeeProductTile extends StatelessWidget {
+  final Coffee coffeeProduct;
+  final void Function() onAddPressed;
+  const CoffeeProductTile({
+    super.key,
+    required this.coffeeProduct,
+    required this.onAddPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey[400],
+        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+      ),
+      margin: const EdgeInsets.only(bottom: 15.0),
+      padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 10),
+      child: ListTile(
+        title: Text(coffeeProduct.name),
+        subtitle: Text(coffeeProduct.price),
+        leading: Image(
+          image: coffeeProduct.coffeeImage,
+          width: 70.0,
+          height: 70.0,
+        ),
+        trailing: IconButton(icon: Icon(Icons.add), onPressed: onAddPressed),
+      ),
+    );
+  }
+}
