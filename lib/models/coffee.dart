1d730aba498a894cb8026099b40f1014a821c20b
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2025-12-17 06:19:03 +0000

    fix: ressolve wrong datatype for coffee image in coffee data model

--- lib/models/coffee.dart
+++ lib/models/coffee.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/material.dart';
+
 class Coffee {
   final String name;
   final String price;
-  final String coffeeImage;
+  final AssetImage coffeeImage;
 
   Coffee({required this.name, required this.price, required this.coffeeImage});
 }
