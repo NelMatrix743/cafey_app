40e2cdd35fe6dde994b26f8133ad704f2061faff
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2025-12-17 06:53:19 +0000

    feat(data): implement coffee store data model

---
+++ lib/models/coffe_store.dart
@@ -0,0 +1,26 @@
+import "../utils/image_assets.dart";
+import "./coffee.dart";
+
+class CoffeeStore {
+  static final List<Coffee> _availableCoffeeProducts = <Coffee>[
+    Coffee(
+      name: "Long Black",
+      price: "4.00",
+      coffeeImage: longBlackSharpAssetIcon,
+    ),
+    Coffee(
+      name: "Espresso",
+      price: "3.00",
+      coffeeImage: espressoSharpAssetIcon,
+    ),
+    Coffee(name: "Latte", price: "5.00", coffeeImage: latteSharpAssetIcon),
+    Coffee(
+      name: "Iced Coffee",
+      price: "4.50",
+      coffeeImage: icedCoffeeSharpAssetIcon,
+    ),
+  ];
+
+  static List<Coffee> get getCoffeeShopProducts =>
+      List.unmodifiable(_availableCoffeeProducts);
+}
