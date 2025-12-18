393b09dbea415c1912ca1aced107b62d7699f708
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2025-12-18 13:52:04 +0000

    refactor: rename the screen sections source files accordingly

---
+++ lib/screens/sections/shopping_section.dart
@@ -0,0 +1,64 @@
+import "package:cafey_app/components/coffee_tile.dart";
+import "package:cafey_app/models/coffe_store.dart";
+import "package:cafey_app/models/coffee.dart";
+import "package:cafey_app/models/user_cart.dart";
+import "package:flutter/material.dart";
+import "package:provider/provider.dart";
+import "../../utils/strings.dart";
+
+class ShoppingCartSection extends StatefulWidget {
+  const ShoppingCartSection({super.key});
+
+  @override
+  State<ShoppingCartSection> createState() => _ShoppingCartSectionState();
+}
+
+class _ShoppingCartSectionState extends State<ShoppingCartSection> {
+  final List<Coffee> allAvailableCoffee = CoffeeStore.getCoffeeShopProducts;
+
+  void addCoffeeProductToCart(Coffee userCoffeeChoice) {
+    Provider.of<UserCart>(
+      context,
+      listen: false,
+    ).addItemToCart(userCoffeeChoice);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<UserCart>(
+      builder: (context, value, child) => SafeArea(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(15.0, 20.0, 15.0, 10.0),
+          child: Column(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.centerLeft,
+                child: const Text(
+                  coffeePresentationQuestion,
+                  style: TextStyle(
+                    fontSize: 17,
+                    // fontFamily: "Consolas",
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: allAvailableCoffee.length,
+                  itemBuilder: (context, index) {
+                    Coffee coffee = allAvailableCoffee[index];
+                    return CoffeeProductTile(
+                      coffeeProduct: coffee,
+                      onAddPressed: () => addCoffeeProductToCart(coffee),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
