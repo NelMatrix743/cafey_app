4baad47d097c38f428e471d971b4242bee9e37db
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2025-12-18 14:15:40 +0000

    feat(ui): add core class for cart content tile

---
+++ lib/components/cart_coffee_tile.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class CartContentTile extends StatefulWidget {
+  const CartContentTile({super.key});
+
+  @override
+  State<CartContentTile> createState() => _CartContentTileState();
+}
+
+class _CartContentTileState extends State<CartContentTile> {
+  @override
+  Widget build(BuildContext context) {
+    return const ListTile();
+  }
+}
