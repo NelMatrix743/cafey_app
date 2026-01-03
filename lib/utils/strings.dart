c4794fa0934362ee4f17492bf4bcbbf32bcd657d
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-01-03 16:52:55 +0000

    feat: update strings.dart to include information labels

--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -1,1 +1,3 @@
-const coffeePresentationQuestion = "What Coffee Would You Like Today?";
+const String coffeePresentationQuestion = "What Coffee Would You Like Today?";
+const String totalAmountInfoLabel = "Amount(\$): ";
+const String totalNumberOfCupsLabel = "Total Cups: ";
