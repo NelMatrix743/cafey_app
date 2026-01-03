ec34a394791c8fcb5a830600a88e97bda85128f7
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-01-03 19:09:55 +0000

    feat(ui): implement user cart section

--- lib/screens/sections/cart_section.dart
+++ lib/screens/sections/cart_section.dart
@@ -1,15 +1,39 @@
 import "package:flutter/material.dart";
+import "package:provider/provider.dart";
 
-class CartSection extends StatefulWidget {
+import "package:cafey_app/models/coffee.dart";
+import "package:cafey_app/models/user_cart.dart";
+import "package:cafey_app/components/info_header.dart";
+import "package:cafey_app/components/cart_coffee_tile.dart";
+
+class CartSection extends StatelessWidget {
   const CartSection({super.key});
 
   @override
-  State<CartSection> createState() => _CartSectionState();
-}
-
-class _CartSectionState extends State<CartSection> {
-  @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Cart Section"));
+    return Consumer<UserCart>(
+      builder: (context, userCart, child) => SafeArea(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(15.0, 20.0, 15.0, 10.0),
+          child: Column(
+            children: [
+              InformationHeader(),
+              SizedBox(height: 30),
+              // ListBuilder,
+              ListView.builder(
+                itemCount: userCart.getUserCartContents.length,
+                itemBuilder: (context, index) {
+                  final entry = userCart.getUserCartContents.entries.elementAt(
+                    index,
+                  );
+                  final Coffee coffee = entry.key;
+                  return CartContentTile(selectedCoffee: coffee);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
