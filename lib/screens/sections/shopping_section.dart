fca4d431e706e0caf658dd01d4fc03c4f07aa575
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-01-03 19:24:22 +0000

    feat(ui): improve addcoffeeproducttocart method to update user cart and balance accordingly

--- lib/screens/sections/shopping_section.dart
+++ lib/screens/sections/shopping_section.dart
@@ -1,10 +1,12 @@
+import "package:cafey_app/models/user_balance.dart";
+import "package:flutter/material.dart";
+import "package:provider/provider.dart";
+
 import "package:cafey_app/components/shop_coffee_tile.dart";
 import "package:cafey_app/models/coffe_store.dart";
 import "package:cafey_app/models/coffee.dart";
 import "package:cafey_app/models/user_cart.dart";
-import "package:flutter/material.dart";
-import "package:provider/provider.dart";
-import "../../utils/strings.dart";
+import "package:cafey_app/utils/strings.dart";
 
 class ShoppingSection extends StatefulWidget {
   const ShoppingSection({super.key});
@@ -16,17 +18,24 @@ class ShoppingSection extends StatefulWidget {
 class _ShoppingSectionState extends State<ShoppingSection> {
   final List<Coffee> allAvailableCoffee = CoffeeStore.getCoffeeShopProducts;
 
-  void addCoffeeProductToCart(Coffee userCoffeeChoice) {
-    Provider.of<UserCart>(
-      context,
-      listen: false,
-    ).addItemToCart(userCoffeeChoice);
+  void addCoffeeProductToCart(
+    Coffee userCoffeeChoice,
+    UserCart cart,
+    UserBalance balance,
+  ) {
+    if (cart.getUserCartContents.containsKey(userCoffeeChoice)) {
+      return; // TODO: show a toast
+    }
+    cart.addItemToCart(userCoffeeChoice);
+    balance.addCoffee(double.parse(userCoffeeChoice.price));
   }
 
   @override
   Widget build(BuildContext context) {
+    final UserBalance userBalance = context.watch<UserBalance>();
+
     return Consumer<UserCart>(
-      builder: (context, value, child) => SafeArea(
+      builder: (context, userCart, child) => SafeArea(
         child: Padding(
           padding: EdgeInsets.fromLTRB(15.0, 20.0, 15.0, 10.0),
           child: Column(
@@ -50,7 +59,8 @@ class _ShoppingSectionState extends State<ShoppingSection> {
                     Coffee coffee = allAvailableCoffee[index];
                     return CoffeeProductTile(
                       coffeeProduct: coffee,
-                      onAddPressed: () => addCoffeeProductToCart(coffee),
+                      onAddPressed: () =>
+                          addCoffeeProductToCart(coffee, userCart, userBalance),
                     );
                   },
                 ),
