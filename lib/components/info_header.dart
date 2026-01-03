2f70c2eda974d721d8624d7c731d70fac28f0b3d
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-01-03 17:09:29 +0000

    feat(ui): turn info header to stateless widget and make it consume the user balance provider

--- lib/components/info_header.dart
+++ lib/components/info_header.dart
@@ -1,34 +1,31 @@
 import "package:flutter/material.dart";
+import "package:provider/provider.dart";
 
 import "package:cafey_app/components/info_box.dart";
 import "package:cafey_app/utils/strings.dart";
+import "package:cafey_app/models/user_balance.dart";
 
-class InformationHeader extends StatefulWidget {
+class InformationHeader extends StatelessWidget {
   const InformationHeader({super.key});
 
   @override
-  State<InformationHeader> createState() => _InformationHeaderState();
-}
-
-class _InformationHeaderState extends State<InformationHeader> {
-  double totalAmount = 12.5;
-  int totalNumberOfCups = 5;
-  @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 12.0, bottom: 12.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          InfoBox(
-            infoType: totalAmountInfoLabel,
-            value: totalAmount.toString(),
-          ),
-          InfoBox(
-            infoType: totalNumberOfCupsLabel,
-            value: totalNumberOfCups.toString(),
-          ),
-        ],
+    return Consumer<UserBalance>(
+      builder: (context, balance, child) => Container(
+        padding: EdgeInsets.only(top: 12.0, bottom: 12.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            InfoBox(
+              infoType: totalAmountInfoLabel,
+              value: balance.totalAmountToPay.toString(),
+            ),
+            InfoBox(
+              infoType: totalNumberOfCupsLabel,
+              value: balance.totalNumberOfCoffeeCups.toString(),
+            ),
+          ],
+        ),
       ),
     );
   }
