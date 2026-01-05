2ddf9b7316ece33012623fa3cf7dc204933ba0ea
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-01-05 08:31:47 +0000

    feat(ui): implement feature to remove item completely from cart section

--- lib/screens/sections/cart_section.dart
+++ lib/screens/sections/cart_section.dart
@@ -3,14 +3,27 @@ import "package:provider/provider.dart";
 
 import "package:cafey_app/models/coffee.dart";
 import "package:cafey_app/models/user_cart.dart";
+import "package:cafey_app/models/user_balance.dart";
 import "package:cafey_app/components/info_header.dart";
 import "package:cafey_app/components/cart_coffee_tile.dart";
 
 class CartSection extends StatelessWidget {
   const CartSection({super.key});
 
+  void removeThisCoffeeCompletelyFromCart(
+    UserCart userCart,
+    UserBalance userBalance,
+    Coffee selectedCoffee,
+  ) {
+    int numberOfCups = userCart.removeItemFromCartCompletely(selectedCoffee);
+    double totalPriceToDeduct =
+        double.parse(selectedCoffee.price) * numberOfCups;
+    userBalance.removeCoffee(totalPriceToDeduct, numberOfCups);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final UserBalance userBalance = context.read<UserBalance>();
     return Consumer<UserCart>(
       builder: (context, userCart, child) => SafeArea(
         child: Padding(
@@ -29,7 +42,15 @@ class CartSection extends StatelessWidget {
                           final entry = userCart.getUserCartContents.entries
                               .elementAt(index);
                           final Coffee coffee = entry.key;
-                          return CartContentTile(selectedCoffee: coffee);
+                          return CartContentTile(
+                            selectedCoffee: coffee,
+                            deleteAction: () =>
+                                removeThisCoffeeCompletelyFromCart(
+                                  userCart,
+                                  userBalance,
+                                  coffee,
+                                ),
+                          );
                         },
                         separatorBuilder: (context, index) =>
                             SizedBox(height: 15),
