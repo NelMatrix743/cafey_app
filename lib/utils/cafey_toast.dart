4a80a99b45c92de376818e678c75f33c77708bed
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-01-05 10:52:39 +0000

    feat(ui): implement function for showing cafey toast

---
+++ lib/utils/cafey_toast.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import "package:delightful_toast/delight_toast.dart";
+import 'package:delightful_toast/toast/components/toast_card.dart';
+
+import "package:cafey_app/utils/image_assets.dart";
+
+void showCafeyToast(BuildContext context, String message) {
+  DelightToastBar(
+    builder: (context) {
+      return ToastCard(
+        title: Center(
+          child: Text(
+            message,
+            style: TextStyle(fontWeight: FontWeight.w700, fontSize: 15.0),
+          ),
+        ),
+        leading: const ImageIcon(
+          toastAlertFilledIcon,
+          color: Colors.brown,
+          size: 30,
+        ),
+        trailing: IconButton(
+          icon: const ImageIcon(
+            toastCloseFilledIcon,
+            color: Colors.brown,
+            size: 15,
+          ),
+          padding: EdgeInsets.all(10),
+          onPressed: () {
+            DelightToastBar.removeAll();
+          },
+        ),
+      );
+    },
+    autoDismiss: true,
+    snackbarDuration: Duration(seconds: 5),
+  ).show(context);
+}
